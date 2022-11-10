d56cdab3d3dba69630ed8dc0a7c32df4c9ac7df0
Spagetik <lazo12kirill@gmail.com>
2022-11-10 19:12:30 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,351 @@
+/**
+ * Kostra programu pro 2. projekt IZP 2022/23
+ *
+ * Jednoducha shlukova analyza: 2D nejblizsi soused.
+ * Single linkage
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h> // sqrtf
+#include <limits.h> // INT_MAX
+#include <string.h>
+
+/*****************************************************************
+ * Ladici makra. Vypnout jejich efekt lze definici makra
+ * NDEBUG, napr.:
+ *   a) pri prekladu argumentem prekladaci -DNDEBUG
+ *   b) v souboru (na radek pred #include <assert.h>
+ *      #define NDEBUG
+ */
+#ifdef NDEBUG
+#define debug(s)
+#define dfmt(s, ...)
+#define dint(i)
+#define dfloat(f)
+#else
+
+// vypise ladici retezec
+#define debug(s) printf("- %s\n", s)
+
+// vypise formatovany ladici vystup - pouziti podobne jako printf
+#define dfmt(s, ...) printf(" - "__FILE__":%u: "s"\n",__LINE__,__VA_ARGS__)
+
+// vypise ladici informaci o promenne - pouziti dint(identifikator_promenne)
+#define dint(i) printf(" - " __FILE__ ":%u: " #i " = %d\n", __LINE__, i)
+
+// vypise ladici informaci o promenne typu float - pouziti
+// dfloat(identifikator_promenne)
+#define dfloat(f) printf(" - " __FILE__ ":%u: " #f " = %g\n", __LINE__, f)
+
+#endif
+
+/*****************************************************************
+ * Deklarace potrebnych datovych typu:
+ *
+ * TYTO DEKLARACE NEMENTE
+ *
+ *   struct obj_t - struktura objektu: identifikator a souradnice
+ *   struct cluster_t - shluk objektu:
+ *      pocet objektu ve shluku,
+ *      kapacita shluku (pocet objektu, pro ktere je rezervovano
+ *          misto v poli),
+ *      ukazatel na pole shluku.
+ */
+
+struct obj_t {
+    int id;
+    float x;
+    float y;
+};
+
+struct cluster_t {
+    int size;
+    int capacity;
+    struct obj_t *obj;
+};
+
+/*****************************************************************
+ * Deklarace potrebnych funkci.
+ *
+ * PROTOTYPY FUNKCI NEMENTE
+ *
+ * IMPLEMENTUJTE POUZE FUNKCE NA MISTECH OZNACENYCH 'TODO'
+ *
+ */
+
+/*
+ Inicializace shluku 'c'. Alokuje pamet pro cap objektu (kapacitu).
+ Ukazatel NULL u pole objektu znamena kapacitu 0.
+*/
+void init_cluster(struct cluster_t *c, int cap)
+{
+    assert(c != NULL);
+    assert(cap >= 0);
+
+    c->capacity = cap;
+    c->obj = calloc(cap, sizeof(struct obj_t));
+}
+
+/*
+ Odstraneni vsech objektu shluku a inicializace na prazdny shluk.
+ */
+void clear_cluster(struct cluster_t *c)
+{
+    // TODO
+    assert(c != NULL);
+    c->size = 0;
+    c->capacity = 0;
+    free(c->obj);
+    init_cluster(c, c->capacity);
+}
+
+/// Chunk of cluster objects. Value recommended for reallocation.
+const int CLUSTER_CHUNK = 10;
+
+/*
+ Zmena kapacity shluku 'c' na kapacitu 'new_cap'.
+ */
+struct cluster_t *resize_cluster(struct cluster_t *c, int new_cap)
+{
+    // TUTO FUNKCI NEMENTE
+    assert(c);
+    assert(c->capacity >= 0);
+    assert(new_cap >= 0);
+
+    if (c->capacity >= new_cap)
+        return c;
+
+    size_t size = sizeof(struct obj_t) * new_cap;
+
+    void *arr = realloc(c->obj, size);
+    if (arr == NULL)
+        return NULL;
+
+    c->obj = (struct obj_t*)arr;
+    c->capacity = new_cap;
+    return c;
+}
+
+/*
+ Prida objekt 'obj' na konec shluku 'c'. Rozsiri shluk, pokud se do nej objekt
+ nevejde.
+ */
+void append_cluster(struct cluster_t *c, struct obj_t obj)
+{
+    // TODO
+    assert(c != NULL);
+    if (c->size >= c->capacity) {
+        resize_cluster(c, c->capacity + CLUSTER_CHUNK);
+    }
+    c->obj[c->size] = obj;
+    c->size++;
+}
+
+/*
+ Seradi objekty ve shluku 'c' vzestupne podle jejich identifikacniho cisla.
+ */
+void sort_cluster(struct cluster_t *c);
+
+/*
+ Do shluku 'c1' prida objekty 'c2'. Shluk 'c1' bude v pripade nutnosti rozsiren.
+ Objekty ve shluku 'c1' budou serazeny vzestupne podle identifikacniho cisla.
+ Shluk 'c2' bude nezmenen.
+ */
+void merge_clusters(struct cluster_t *c1, struct cluster_t *c2)
+{
+    assert(c1 != NULL);
+    assert(c2 != NULL);
+
+    // TODO
+}
+
+/**********************************************************************/
+/* Prace s polem shluku */
+
+/*
+ Odstrani shluk z pole shluku 'carr'. Pole shluku obsahuje 'narr' polozek
+ (shluku). Shluk pro odstraneni se nachazi na indexu 'idx'. Funkce vraci novy
+ pocet shluku v poli.
+*/
+int remove_cluster(struct cluster_t *carr, int narr, int idx)
+{
+    assert(idx < narr);
+    assert(narr > 0);
+
+    for (int i = idx; i < narr - 1; i++) {
+        carr[i] = carr[i + 1];
+    }
+
+    return narr - 1;
+
+    // TODO
+}
+
+/*
+ Pocita Euklidovskou vzdalenost mezi dvema objekty.
+ */
+float obj_distance(struct obj_t *o1, struct obj_t *o2)
+{
+    assert(o1 != NULL);
+    assert(o2 != NULL);
+
+    float x1 = o1->x;
+    float y1 = o1->y;
+    float x2 = o2->x;
+    float y2 = o2->y;
+
+    float distance = sqrtf(powf(x2 - x1, 2) + powf(y2 - y1, 2));
+
+    return distance;
+}
+
+/*
+ Pocita vzdalenost dvou shluku.
+*/
+float cluster_distance(struct cluster_t *c1, struct cluster_t *c2)
+{
+    assert(c1 != NULL);
+    assert(c1->size > 0);
+    assert(c2 != NULL);
+    assert(c2->size > 0);
+
+    float min = INFINITY;
+    for (int i = 0; i < c1->size; i++) {
+        for (int j = 0; j < c2->size; j++) {
+            float dist = obj_distance(&c1->obj[i], &c2->obj[j]);
+            if (dist < min) {
+                min = dist;
+            }
+        }
+    }
+    return min;
+}
+
+/*
+ Funkce najde dva nejblizsi shluky. V poli shluku 'carr' o velikosti 'narr'
+ hleda dva nejblizsi shluky. Nalezene shluky identifikuje jejich indexy v poli
+ 'carr'. Funkce nalezene shluky (indexy do pole 'carr') uklada do pameti na
+ adresu 'c1' resp. 'c2'.
+*/
+void find_neighbours(struct cluster_t *carr, int narr, int *c1, int *c2)
+{
+    assert(narr > 0);
+
+    float min = INFINITY;
+    for (int i = 0; i < narr; i++) {
+        for (int j = i + 1; j < narr; j++) {
+            float dist = cluster_distance(&carr[i], &carr[j]);
+            if (dist < min) {
+                min = dist;
+                *c1 = i;
+                *c2 = j;
+            }
+        }
+    }
+
+    // TODO
+}
+
+// pomocna funkce pro razeni shluku
+static int obj_sort_compar(const void *a, const void *b)
+{
+    // TUTO FUNKCI NEMENTE
+    const struct obj_t *o1 = (const struct obj_t *)a;
+    const struct obj_t *o2 = (const struct obj_t *)b;
+    if (o1->id < o2->id) return -1;
+    if (o1->id > o2->id) return 1;
+    return 0;
+}
+
+/*
+ Razeni objektu ve shluku vzestupne podle jejich identifikatoru.
+*/
+void sort_cluster(struct cluster_t *c)
+{
+    // TUTO FUNKCI NEMENTE
+    qsort(c->obj, c->size, sizeof(struct obj_t), &obj_sort_compar);
+}
+
+/*
+ Tisk shluku 'c' na stdout.
+*/
+void print_cluster(struct cluster_t *c)
+{
+    // TUTO FUNKCI NEMENTE
+    for (int i = 0; i < c->size; i++)
+    {
+        if (i) putchar(' ');
+        printf("%d[%g,%g]", c->obj[i].id, c->obj[i].x, c->obj[i].y);
+    }
+    putchar('\n');
+}
+
+/*
+ Ze souboru 'filename' nacte objekty. Pro kazdy objekt vytvori shluk a ulozi
+ jej do pole shluku. Alokuje prostor pro pole vsech shluku a ukazatel na prvni
+ polozku pole (ukalazatel na prvni shluk v alokovanem poli) ulozi do pameti,
+ kam se odkazuje parametr 'arr'. Funkce vraci pocet nactenych objektu (shluku).
+ V pripade nejake chyby uklada do pameti, kam se odkazuje 'arr', hodnotu NULL.
+*/
+int load_clusters(char *filename, struct cluster_t **arr)
+{
+    assert(arr != NULL);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: File %s could not be opened.\n", filename);
+        return -1;
+    }
+    char buffer[101];
+    fgets(buffer, 100, file);
+    char *endPt = strchr(buffer, '=');
+    int count = strtol(endPt+1, NULL, 10);
+    int i = 0;
+    while (fgets(buffer, 100, file) != NULL && i < count)
+    {
+        int id, x, y;
+        id = strtol(buffer, &endPt, 10);
+        x = strtol(endPt, &endPt, 10);
+        y = strtol(endPt, NULL, 10);
+        dfmt("id: %d, x: %d, y: %d", id, x, y);
+    }
+    return count;
+}
+
+/*
+ Tisk pole shluku. Parametr 'carr' je ukazatel na prvni polozku (shluk).
+ Tiskne se prvnich 'narr' shluku.
+*/
+void print_clusters(struct cluster_t *carr, int narr)
+{
+    printf("Clusters:\n");
+    for (int i = 0; i < narr; i++)
+    {
+        printf("cluster %d: ", i);
+        print_cluster(&carr[i]);
+    }
+}
+
+void parse_args(int argc, char *argv[], int *n, char **filename) {
+    if (argc == 2) {
+        *filename = argv[1];
+        *n = 1;
+    } else if (argc == 3) {
+        *filename = argv[1];
+        *n = atoi(argv[2]);
+    } else {
+        fprintf(stderr, "Invalid arguments");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    struct cluster_t *clusters;
+    int cluster_amount;
+    char *filename;
+    parse_args(argc, argv, &cluster_amount, &filename);
+    load_clusters(filename, &clusters);
+    return 0;
+}
